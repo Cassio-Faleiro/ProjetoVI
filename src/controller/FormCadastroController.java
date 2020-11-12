e6e64ddb8ab6cb78bb7897fea6e670f36a432f15
Andre <andre.mallmann@universo.univates.br>
2020-11-12 17:36:54 -0300

    fazendo funcionar tela de cadastro e login com autenticacao no banco

--- src/controller/FormCadastroController.java
+++ src/controller/FormCadastroController.java
@@ -8,6 +8,7 @@ package controller;
 import dao.Conexao;
 import dao.UsuarioDao;
 import interfaces.frmLogin;
+import interfaces.frmPrincipal;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.logging.Level;
@@ -38,12 +39,35 @@ public class FormCadastroController {
             UsuarioDao usuarioDao = new UsuarioDao(conexao);
             if (senha == null ? senhaConfirmacao == null : senha.equals(senhaConfirmacao)) {
             usuarioDao.insert(usuario1);
+            
+                JOptionPane.showMessageDialog(null, "Salvo com sucesso");
             } else {
                 JOptionPane.showMessageDialog(null, "Senhas não são iguais");
             }
         } catch (SQLException ex) {
             Logger.getLogger(frmLogin.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    public void autenticar() throws SQLException {
+        //buscar usuario da view
+        String usuario = view.getTxtLogin().getText();
+        String senhaLogin = view.getPsfSenha3().getText();
+        Usuario usuarioAutenticar = new Usuario(usuario, senhaLogin);
+        //verifica se existe no BD
+        
+        Connection conexao = new Conexao().getConnection();
+        UsuarioDao usuarioDao = new UsuarioDao(conexao);
+        
+        boolean existe = usuarioDao.existePorUsuarioESenha(usuarioAutenticar);
+        
+        if(existe) {
+            frmPrincipal principal = new frmPrincipal();
+            principal.setVisible(true);
+        } else {
+            JOptionPane.showMessageDialog(view, "Usuario não encontrado");
+        }
+        
     }
     
 }
--- src/dao/UsuarioDao.java
+++ src/dao/UsuarioDao.java
@@ -4,6 +4,7 @@ package dao;
 import interfaces.frmLogin;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,6 +23,17 @@ public class UsuarioDao {
             String sql = "INSERT INTO login (usuario, senha) values('"+usuario.getUsuario()+"', '"+usuario.getSenha()+"');";
             PreparedStatement statement = connection.prepareStatement(sql);
             statement.execute();
-            connection.close();
+    }
+
+    public boolean existePorUsuarioESenha(Usuario usuarioAutenticar) throws SQLException {
+            String sql = "SELECT * FROM login WHERE usuario = '"+usuarioAutenticar.getUsuario()+"' and senha= '"+usuarioAutenticar.getSenha()+"'";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.execute();
+            
+            ResultSet resultSet = statement.getResultSet();
+            
+            //varre a linha para verificar se existe algum resultado
+            
+            return resultSet.next();
     }
 }
